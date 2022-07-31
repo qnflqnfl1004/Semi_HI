4e37f553c740f0c145155d8f363c5400b70d7488
DESKTOP-V118V63\javascript <clown5554@naver.com>
2022-07-31 21:51:05 +0900

    20220731 gim

---
+++ HI/src/main/java/com/anmozilla/mvc/member/model/dao/MemberDao.java
@@ -0,0 +1,12 @@
+package com.anmozilla.mvc.member.model.dao;
+
+import com.anmozilla.mvc.member.model.vo.Member;
+
+public class MemberDao {
+
+	public Member findMemberByID(String id) {
+		
+		return null;
+	}
+
+}
